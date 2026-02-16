35dffeee4214c82238563c16169e6a710ec9b532
harshilchandratre <harshilchandratre2002@gmail.com>
2026-02-17 00:55:25 +0530

    add user model with roles

---
+++ server/src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      select: false,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "manager", "member"],
+      default: "member",
+    },
+    workspaceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
